3bd1c9422ef76c9f30046475d691700ccd7e5a00
Humblefool94 <akanth1994@gmail.com>
2018-11-13 13:34:22 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
-//Install express server
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/AbhishekEkaanth'));
+
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/AbhishekEkaanth/dist/index.html'));
+// });
+
+
+// // app.get('*', function(req, res) {
+// //     const index = path.join(__dirname, 'dist', 'index.html');
+// //     res.sendFile(index);
+// // })
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/AbhishekEkaanth'));
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + 'AbhishekEkaanth/dist/index.html'));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-
-// app.get('*', function(req, res) {
-//     const index = path.join(__dirname, 'dist', 'index.html');
-//     res.sendFile(index);
-// })
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
