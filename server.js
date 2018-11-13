25cac525d0e2bb6d708dd2b244509d113167f857
Humblefool94 <akanth1994@gmail.com>
2018-11-13 13:42:07 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,40 +1,16 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/AbhishekEkaanth'));
-
-// app.get('/*', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/AbhishekEkaanth/dist/index.html'));
-// });
-
-
-// // app.get('*', function(req, res) {
-// //     const index = path.join(__dirname, 'dist', 'index.html');
-// //     res.sendFile(index);
-// // })
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
+//Install express server
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/AbhishekEkaanth'));
 
+app.get('/*', function(req, res) {
 
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/AbhishekEkaanth/index.html'));
 });
-
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
